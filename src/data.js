0c2ff74d61ff0fc7566c6c35f9890aeba3350b77
Dani1592 <danielagutierrez1592@gmail.com>
2018-07-01 20:45:28 -0500

    cambios para priemra funcion,entrada a sedes y generaciones  mas cmabios de css

--- src/data.js
+++ src/data.js
@@ -1,18 +1,22 @@
 let laboratoria = fetch('https://raw.githubusercontent.com/Dani1592/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json');
 
-window.computeStudentsStats= (laboratoria) => {
-
+window.computeStudentsStats= (laboratoria,pais) => {
     laboratoria.then(function (res) {
         return res.json();
 
     }).then(function (dato) {
-        let sedeOption = dato.lima;
-        let sedeSave = '';
-        sedeOption.forEach(element => {
-            sedeSave += `<li>${element.generacion}</li>`;
-            console.log(element.generacion);
-        });
+      //console.log(dato[pais].generacion) asi entras aun objeto por medio de una variable :)
+        let sedeOption = dato[pais].generacion;
+        //console.log(sedeOption)
+        Object.keys(sedeOption).forEach( (key) =>{
+      //    console.log(key) aqui recorre cada uno de los objetos
+          let newElement = document.createElement("option");//creas un elemento opcion
+          newElement.setAttribute("value",key);// le creas un valor al elelmento
+          newElement.appendChild(document.createTextNode(key + " generación"))//le creas un hijo con texto
+          document.getElementById("generacion").appendChild(newElement);
+      //le creas un hijo al padre generacion
     })
+  })
 };
 
 window.computeGenerationsStats= (laboratoria) => {
