4677973156eab68a7463c6ccf70c5f9e8bc82da6
Dani1592 <danielagutierrez1592@gmail.com>
2018-07-02 21:31:44 -0500

    funcion donde interactuamos con los alunas email y programas

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+let laboratoria = 'https://raw.githubusercontent.com/Dani1592/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json';
+
 let body = document.getElementById('cuerpo');
 
 let menu = document.getElementById('menu');
@@ -17,11 +19,28 @@ buton.addEventListener('click',access);
 function access() {
     body.style.display = 'none';
     menu.style.display = 'inline-block';
+    sedeCreate()
 };
 
 let sede1 = document.getElementById('sede');
 
+
 sede1.addEventListener('change',sedeSelect);
+function sedeCreate (){
+  fetch(laboratoria).then(function (res) {
+    return res.json();
+
+  }).then(function (dato){
+    console.log(dato);
+    let sedeOption = dato;
+    Object.keys(sedeOption).forEach((sede) =>{
+      let sedePais = document.createElement("option");
+      sedePais.setAttribute("value",sede);
+      sedePais.appendChild(document.createTextNode(sede));
+      document.getElementById("sede").appendChild(sedePais);
+})
+}  )
+}
 
 function sedeSelect () {
 
@@ -29,11 +48,12 @@ function sedeSelect () {
     menu2.style.display = 'inline-block';
 
     if (sede == 'lima') {
-        let data = document.getElementById('data').innerHTML=computeStudentsStats(laboratoria,'lima');
-    } else if (sede == 'cdmx') {
-        let data = document.getElementById('data').innerHTML=computeStudentsStats(laboratoria,'mexico');
+        let data = document.getElementById('data').innerHTML = computeStudentsStats(laboratoria,'lima');
+    } else if (sede == 'mexico') {
+        let data = document.getElementById('data').innerHTML = computeStudentsStats(laboratoria,'mexico');
     } else {
-        let data = document.getElementById('data').innerHTML=computeStudentsStats(laboratoria,'santiago');
+        let data = document.getElementById('data').innerHTML = computeStudentsStats(laboratoria,'santiago');
+
     }
 };
 
@@ -44,12 +64,9 @@ generation1.addEventListener('change',generationSelect);
 function generationSelect () {
 
     let generation = generation1.value;
+//console.log(generation)
+  let sede = document.getElementById("sede").value;
+  //console.log(sede)
+  let result  = document.getElementById('resultadosGeneration').innerHTML = computeGenerationsStats(laboratoria,generation,sede);
 
-    if (generation == 'quinta') {
-    
-    } else if (generation == 'cuarta') {
-
-    } else {
-
-    }
 };
