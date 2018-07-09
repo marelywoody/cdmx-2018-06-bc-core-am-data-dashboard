be879e2ef93ec80d2f8ceebb0cf169428a237a1e
Dani1592 <danielagutierrez1592@gmail.com>
2018-07-08 19:07:02 -0500

    primera funcion terminada sin css

--- src/data.js
+++ src/data.js
@@ -1,5 +1,5 @@
 window.computeStudentsStats= (laboratoria,generacion,sede) => {
-console.log(laboratoria,generacion,sede);
+//console.log(laboratoria,generacion,sede);
 fetch(laboratoria).then(function (res) {
 return res.json();
 })
@@ -9,24 +9,93 @@ return res.json();
     Object.keys(estudiantesOption).forEach ((estudent)=>{
       let estudiante = estudiantesOption[estudent];
       let cajaEstudiante = document.createElement("article");
-console.log(cajaEstudiante)
+      //console.log(cajaEstudiante)
       cajaEstudiante.setAttribute("class","jumbotron");
      //crear nombre
-      let nombre1 = document.createElement("h6");
-      nombre1.appendChild(document.createTextNode('name: '+ estudiante.nombre));
+      let nombre1 = document.createElement("p");
+      nombre1.appendChild(document.createTextNode('Name: '+ estudiante.nombre));
       cajaEstudiante.appendChild(nombre1);
 
       let correo = document.createElement("p");
-      correo.appendChild(document.createTextNode('email: ' + estudiante.correo));
+      correo.appendChild(document.createTextNode('Email: ' + estudiante.correo));
       cajaEstudiante.appendChild(correo);
 
-      let programDuration = document.createElement("article");
-      programDuration .appendChild(document.createTextNode('Duracion de programa: '+ estudiante.progreso.duracionPrograma));
+      let programDuration = document.createElement("p");
+      programDuration.appendChild(document.createTextNode('Duracion de programa: '+ estudiante.progreso.duracionPrograma));
       cajaEstudiante.appendChild(programDuration );
 
-      document.
-     getElementById('resultadosGeneration').appendChild(cajaEstudiante);
-console.log(cajaEstudiante)
+      let porcentajeDeDuracion = document.createElement("p");
+      porcentajeDeDuracion.appendChild(document.createTextNode('Porcenta completado: '+ estudiante.progreso.porcentajeCompletado));
+      cajaEstudiante.appendChild(porcentajeDeDuracion );
+
+      let turnoEstudiante = document.createElement("p");
+      turnoEstudiante.appendChild(document.createTextNode('Turno: '+ estudiante.turno));
+      cajaEstudiante.appendChild(turnoEstudiante );
+
+      let tema = document.createElement("articule");
+      tema.appendChild(document.createTextNode('TEMAS'));
+      cajaEstudiante.appendChild(tema);
+
+      let entradaTemas = estudiante.progreso.temas
+      // console.log(entradaTemas)
+      let cajaTemas = document.createElement("articule")
+      cajaEstudiante.appendChild(cajaTemas);
+
+      Object.keys(entradaTemas).forEach( (tema)=>{
+      let entrandoTemas = entradaTemas[tema];
+      //console.log(entrandoTemas)
+
+      let tema1= document.createElement("p");
+      tema1.appendChild(document.createTextNode('Tema: '+ tema));
+      cajaTemas.appendChild(tema1);
+
+      let porcentajeCompletado = document.createElement("p");
+      porcentajeCompletado.appendChild(document.createTextNode("Porcentaje Completado: "+entrandoTemas.porcentajeCompletado))
+      cajaTemas.appendChild(porcentajeCompletado);
+      //console.log(porcentajeCompletado)
+
+      let duracionTemaCompletado = document.createElement("p");
+      duracionTemaCompletado.appendChild(document.createTextNode("Duracion de Tema Completado: "+ entrandoTemas.duracionTemaCompletado))
+      cajaTemas.appendChild(duracionTemaCompletado)
+        let  entradaSubtemas = entrandoTemas.subtemas
+      //console.log(entradaSubtemas)
+        let cajaSubtemas = document.createElement("articule")
+        cajaTemas.appendChild(cajaSubtemas)
+
+        Object.keys(entradaSubtemas).forEach((subtemas)=>{
+          let entrandoSubtemas = entradaSubtemas[subtemas];
+          console.log((entradaSubtemas))
+
+          let subtemas1 = document.createElement("p")
+          subtemas1.appendChild(document.createTextNode(subtemas))
+          cajaSubtemas.appendChild(subtemas1)
+          console.log(subtemas1)
+
+          let duracionDeTema =document.createElement("p")
+          duracionDeTema.appendChild(document.createTextNode("Duracion: " + entrandoSubtemas.duracionSubtema))
+          cajaSubtemas.appendChild(duracionDeTema)
+
+          let tipo =document.createElement("p")
+          tipo.appendChild(document.createTextNode("Tipo: " + entrandoSubtemas.tipo))
+          cajaSubtemas.appendChild(tipo)
+
+          let completado = document.createElement("p")
+          completado.appendChild(document.createTextNode("Completado: " + entrandoSubtemas.completado))
+          cajaSubtemas.appendChild(completado)
+
+
+      })
+
+
+
+
+    })
+
+
+
+
+      document.getElementById('resultadosGeneration').appendChild(cajaEstudiante);
+      //console.log(cajaEstudiante)
 
 
     })
@@ -40,6 +109,8 @@ window.computeGenerationsStats= (laboratoria,pais) => {
  .then(function (dato) {
    //console.log(document.getElementById("generacion").hasChildNodes())
    let generacionSelect = document.getElementById('generacion');
+
+
    if (generacionSelect.hasChildNodes()){
 
      while(generacionSelect.childNodes.length >= 1){
@@ -48,6 +119,11 @@ window.computeGenerationsStats= (laboratoria,pais) => {
      }
 
    }
+   let newElement = document.createElement("option");//creas un elemento opcion
+   newElement.setAttribute("value","-1");// le creas un valor al elelmento
+   newElement.appendChild(document.createTextNode("Selecciona una generación"))//le creas un hijo con texto
+   document.getElementById("generacion").appendChild(newElement);
+
     let generacionOption = dato[pais].generacion;
         //console.log(generacionOption)
         Object.keys(generacionOption).forEach( (key) =>{
