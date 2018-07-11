49cfe3db0628ca74a1b33b8032fa52bed1bb4daf
marelywoody <ros_ely_arc@hotmail.com>
2018-07-11 08:15:19 -0500

    acomodamiento slect

--- src/main.js
+++ src/main.js
@@ -76,43 +76,40 @@ getGeneration = (data) => {
   });
 };
 drawStudents = (data) => {
-let cajaGeneraciones =   document.getElementById('resultadosGeneration')
-
-if(cajaGeneraciones.hasChildNodes()) {
-
-  while(cajaGeneraciones.childNodes.length >= 1){
-    cajaGeneraciones.removeChild(cajaGeneraciones.firstChild);
-  }}
-let sede = document.getElementById('sede').value;
-let generation = document.getElementById('generacion').value;
-var estudianteSede = data.filter((estudiante) => {
-    if(estudiante.campus ==  sede && estudiante.generation == generation
-    ){
- return estudiante
+  let cajaGeneraciones = document.getElementById('resultadosGeneration');
+  
+  if (cajaGeneraciones.hasChildNodes()) {
+    while (cajaGeneraciones.childNodes.length >= 1) {
+      cajaGeneraciones.removeChild(cajaGeneraciones.firstChild);
+    }
   }
-})
-estudianteSede.forEach((estudiante)=>{
+  let sede = document.getElementById('sede').value;
+  let generation = document.getElementById('generacion').value;
+  var estudianteSede = data.filter((estudiante) => {
+    if (estudiante.campus == sede && estudiante.generation == generation) {
+      return estudiante;
+    }
+  });
+  estudianteSede.forEach((estudiante) => {
+    let cajaEstudiante = document.createElement('article');
+    // console.log(cajaEstudiante)
+    cajaEstudiante.setAttribute('class', 'jumbotron');
+  
+    let nombre1 = document.createElement('p');
+    nombre1.appendChild(document.createTextNode('Name: ' + estudiante.name));
+    cajaEstudiante.appendChild(nombre1);
+    let correo = document.createElement('p');
+    correo.appendChild(document.createTextNode('Email: ' + estudiante.email));
+    cajaEstudiante.appendChild(correo);
+  
+    let generacion = document.createElement('p');
+    generacion.appendChild(document.createTextNode('Generación: ' + estudiante.generation));
+    cajaEstudiante.appendChild(generacion);
+  
+    let campus = document.createElement('p');
+    campus.appendChild(document.createTextNode('Campus: ' + estudiante.campus));
+    cajaEstudiante.appendChild(campus);
+    cajaGeneraciones.appendChild(cajaEstudiante);
+  });
+};
 
-  let cajaEstudiante = document.createElement("article");
-  // console.log(cajaEstudiante)
-  cajaEstudiante.setAttribute("class","jumbotron");
-
-  let nombre1 = document.createElement("p");
-  nombre1.appendChild(document.createTextNode('Name: '+ estudiante.name));
-  cajaEstudiante.appendChild(nombre1);
-  let correo = document.createElement("p");
-  correo.appendChild(document.createTextNode('Email: ' + estudiante.email));
-  cajaEstudiante.appendChild(correo);
-
-  let generacion = document.createElement("p");
-  generacion.appendChild(document.createTextNode('Generación: '+ estudiante.generation));
-  cajaEstudiante.appendChild(generacion );
-
-  let campus = document.createElement("p");
-  campus.appendChild(document.createTextNode('Campus: '+ estudiante.campus));
-  cajaEstudiante.appendChild(campus );
-
-cajaGeneraciones.appendChild(cajaEstudiante);
-})
-
-}
